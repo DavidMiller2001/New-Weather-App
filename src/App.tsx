971c0db67df2efdde3a6d753eb1fd88756938c7e
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2024-05-25 18:09:59 -0400

    install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './components/ui/card';
+import './globals.css';
+
+function App() {
+  return (
+    <>
+      <main className='flex h-screen w-screen justify-center items-center bg-slate-400'>
+        <Card>
+          <CardHeader>
+            <CardTitle>New Weather App</CardTitle>
+            <CardDescription>
+              a simple little practice react app made with vite and the
+              openweathermap api!
+            </CardDescription>
+          </CardHeader>
+          <CardContent>Hello</CardContent>
+          <CardFooter>
+            <CardDescription>David Miller 2024</CardDescription>
+          </CardFooter>
+        </Card>
+      </main>
+    </>
+  );
+}
+
+export default App;
